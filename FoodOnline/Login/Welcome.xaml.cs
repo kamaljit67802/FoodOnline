042bbfe8adc6b4c32974d004f85d9339e1496002
rohanyesudasan <rohanyesudasan@gmail.com>
2023-11-09 10:42:41 -0800

    Create Welcome page XAML layout

---
+++ FoodOnline/Login/Welcome.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace FoodOnline.Login;
+
+public partial class Welcome : ContentPage
+{
+    public Welcome()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnMySignUpClicked(object sender, EventArgs e)
+    {       
+
+        App.Current.MainPage = new NavigationPage(new SignUp());
+
+
+    }
+
+    void OnMyLoginClicked(System.Object sender, System.EventArgs e)
+    {
+        App.Current.MainPage = new NavigationPage(new Login());
+
+    }
+}
+

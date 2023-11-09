0374dce1ea79597e47d6b93ba530b036b524b664
rohanyesudasan <rohanyesudasan@gmail.com>
2023-11-09 10:40:01 -0800

    Add Login page UI layout

---
+++ FoodOnline/Login/Login.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FoodOnline.Login;
+public partial class Login : ContentPage
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        // Add your own logic for authenticating the user
+        if (!string.IsNullOrEmpty(EmailEntry.Text) && !string.IsNullOrEmpty(PasswordEntry.Text))
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            await DisplayAlert("Error", "Email and password fields cannot be empty.", "OK");
+        }
+    }
+
+}

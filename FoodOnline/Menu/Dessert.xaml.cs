b4d49beadf4b4f0b535445433c995c29691c89c7
gokulskumar21 <gokulskumar21@gmail.com>
2023-11-20 12:14:20 -0800

    Added Desserts and Dessert Details to the menu

---
+++ FoodOnline/Menu/Dessert.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FoodOnline.Menu;
+using FoodOnline.Models;
+
+
+public partial class Dessert : ContentPage
+{
+	public Dessert()
+	{
+		InitializeComponent();
+
+    }
+
+    private void OnDessertNameTapped(object sender, EventArgs e)
+    {
+        if (sender is Label label && label.BindingContext is Category selectedCategory)
+        {
+            // Handle the tap event, and you have access to the selected category (selectedCategory)
+            // For example, you can navigate to a new page with the selected category
+            Navigation.PushAsync(new DessertDetails(selectedCategory));
+        }
+    }
+}
---
+++ FoodOnline/Menu/DessertDetails.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace FoodOnline.Menu;
+using FoodOnline.Models;
+using FoodOnline.Menu;
+
+public partial class DessertDetails : ContentPage
+{
+
+    public DessertDetails(Category selectedItem)
+    {
+        InitializeComponent();
+        UpdateUI(selectedItem);
+
+    }
+    private void UpdateUI(Category selectedItem)
+    {
+        myDessertName.Text = selectedItem.Name; // Assuming you have a Title property in your Category class
+        myDessertDetails.Text = selectedItem.Name;
+        AddToSelectedItems(selectedItem);
+    }
+    private static void AddToSelectedItems(Category selectedItem)
+    {
+        BurgerDetails.selectedItems.Add(selectedItem);
+    }
+
+
+    private async void OnAddMeToCart(object sender, EventArgs e)
+    {
+
+        DisplayAlert("Selected Items", $"Total selected items: {BurgerDetails.selectedItems.Count}", "OK");
+        Preferences.Default.Remove("Category");
+
+        Navigation.PushAsync(new MainPage());
+
+
+    }
+}

f12d02d24ab6a740ed4131e6eb1d2cd62b26e242
rohanyesudasan <rohanyesudasan@gmail.com>
2023-11-20 12:18:26 -0800

    Added Soup and Soup Details to the Menu

---
+++ FoodOnline/Menu/Soup.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FoodOnline.Menu;
+using FoodOnline.Models;
+
+
+public partial class Soup : ContentPage
+{
+	public Soup()
+	{
+		InitializeComponent();
+
+    }
+
+    private void OnSoupChanged(object sender, EventArgs e)
+    {
+        if (sender is Label label && label.BindingContext is Category selectedCategory)
+        {
+            // Handle the tap event, and you have access to the selected category (selectedCategory)
+            // For example, you can navigate to a new page with the selected category
+            Navigation.PushAsync(new SoupDetails(selectedCategory));
+        }
+    }
+}
